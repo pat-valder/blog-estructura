e258106180fd989ee375ade0a04bdc6e377ad16d
pat-valder <74991022+pat-valder@users.noreply.github.com>
2020-12-19 21:30:20 -0500

    Update config.inc.php

--- app/config.inc.php
+++ app/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 //info de la base de datos
 define('NOMBRE_SERVIDOR', 'localhost');
-define('NOMBRE_USUARIO', 'native');
+define('NOMBRE_USUARIO', 'root');
 define('PASSWORD', '');
 define('NOMBRE_BD', 'blog');
 
